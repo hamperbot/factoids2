30d790a080be3e624dedabe52f39ff192663718e
Mike Cooper <mythmon@gmail.com>
2014-10-16 09:33:08 -0700

    Add entry_points.

--- setup.py
+++ setup.py
@@ -6,10 +6,15 @@ with open('requirements.txt') as f:
 setup(
     name='hamper-factoids2',
     version='0.1',
-    #packages=['hamper_factoids2'],
+    packages=['hamper_factoids2'],
     author='Mike Cooper',
     author_email='mythmon@gmail.com',
     url='https://github.com/mythmon/hamper-factoids2',
     install_requires=requirements,
-    package_data={'hamper-factoids2': ['requirements.txt', 'README.md', 'LICENSE']}
+    package_data={'hamper-factoids2': ['requirements.txt', 'README.md', 'LICENSE']},
+    entry_points={
+        'hamperbot.plugins': [
+            'factoids2 = hamper_factoids.factoids:Factoids',
+        ],
+    },
 )
