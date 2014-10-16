b3debffa781ca6d3136e5ac885d8832d6028c442
Mike Cooper <mythmon@gmail.com>
2014-10-16 09:34:27 -0700

    Make package name consistent.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('requirements.txt') as f:
     requirements = [l.strip() for l in f]
 
 setup(
-    name='hamper-factoids2',
+    name='hamper_factoids',
     version='0.1',
-    packages=['hamper_factoids2'],
+    packages=['hamper_factoids'],
     author='Mike Cooper',
     author_email='mythmon@gmail.com',
     url='https://github.com/mythmon/hamper-factoids2',
     install_requires=requirements,
-    package_data={'hamper-factoids2': ['requirements.txt', 'README.md', 'LICENSE']},
+    package_data={'hamper_factoids': ['requirements.txt', 'README.md', 'LICENSE']},
     entry_points={
         'hamperbot.plugins': [
             'factoids2 = hamper_factoids.factoids:Factoids',
